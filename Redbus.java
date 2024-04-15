21ee11f8919ea10f056a40b62a64e02078c28be3
Hinthumathikarthik <167062942+Hinthumathikarthik@users.noreply.github.com>
2024-04-15 22:00:32 +0530

    Add files via upload

---
+++ Redbus.java
@@ -0,0 +1,22 @@
+package org.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Redbus {
+	
+	public static void main(String[]args) throws InterruptedException{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html");
+		driver.manage().window().maximize();
+		
+		WebElement table = driver.findElement(By.id("customers"));
+		
+		System.out.println(table.getText());
+	
+	
+	}}
